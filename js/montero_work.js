c9bcd1b5eb919e38df7cb77c5406fd23389e013a
Pankhil <pankhilmistry@gmail.com>
2018-12-31 17:24:32 +0530

    iframe test

--- js/montero_work.js
+++ js/montero_work.js
@@ -12,3 +12,38 @@ $('[data-fancybox="Persona-gallery"]').fancybox({
     autoStart : false
   }
 });
+
+
+$('[data-fancybox="ca-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox]').fancybox({
+  toolbar  : false,
+  smallBtn : true,
+  iframe : {
+    preload : false
+  }
+});
+
+$('[data-fancybox="Uiflow-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
