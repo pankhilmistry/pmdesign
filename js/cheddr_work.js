c9a762c7604cb153154f0024634a47c51a156db1
Pankhil <pankhilmistry@gmail.com>
2019-01-01 13:34:43 +0530

    Cheddr Casestudy complete

---
+++ js/cheddr_work.js
@@ -0,0 +1,53 @@
+
+
+$('[data-fancybox="Persona-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="ca-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="Uiflow-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="wireframe-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
