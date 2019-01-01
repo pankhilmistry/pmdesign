34e45d8b73076e77d622f2f8debd7f3130c9871c
Pankhil <pankhilmistry@gmail.com>
2019-01-01 13:40:22 +0530

    cheddr js refactored

--- js/cheddr_work.js
+++ js/cheddr_work.js
@@ -13,33 +13,8 @@ $('[data-fancybox="Persona-gallery"]').fancybox({
   }
 });
 
-$('[data-fancybox="ca-gallery"]').fancybox({
-  buttons : [ 
-    'slideShow',
-    'zoom',
-    'fullScreen',
-    'close',
-    'thumbs'
-  ],
-  thumbs : {
-    autoStart : false
-  }
-});
 
 $('[data-fancybox="Uiflow-gallery"]').fancybox({
-  buttons : [ 
-    'slideShow',
-    'zoom',
-    'fullScreen',
-    'close',
-    'thumbs'
-  ],
-  thumbs : {
-    autoStart : false
-  }
-});
-
-$('[data-fancybox="wireframe-gallery"]').fancybox({
   buttons : [ 
     'slideShow',
     'zoom',
