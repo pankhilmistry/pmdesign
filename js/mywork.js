adc6130fde2f4a5f3a3785c64c9af11871fe4b8d
Pankhil <pankhilmistry@gmail.com>
2019-01-02 21:19:18 +0530

    mywork JS updated

--- js/mywork.js
+++ js/mywork.js
@@ -12,3 +12,119 @@ $('[data-fancybox="APA-gallery"]').fancybox({
     autoStart : false
   }
 });
+
+
+
+$('[data-fancybox="spiro-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
+
+
+$('[data-fancybox="hemo-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
+
+
+$('[data-fancybox="3DP-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
+
+
+
+$('[data-fancybox="vitals-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
+
+
+$('[data-fancybox="vv-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
+
+
+$('[data-fancybox="gd-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
+
+
+$('[data-fancybox="dad-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
+
+
+$('[data-fancybox="rb-gallery"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'zoom',
+    'fullScreen',
+    'close',
+    'thumbs'
+  ],
+  thumbs : {
+    autoStart : false
+  }
+});
+
+
